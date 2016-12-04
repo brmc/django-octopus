eb51c1e98bc1a1dee07a095e0f2ff67ecc5136c6
B Trizzle <brian@mcclure.pw>
2016-12-04 12:38:15 +0100

    fixed deprecated problem

--- octopus/templatetags/a.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from octopus.templatetags.tentacles import *
-
-raise DeprecationWarning("{% load a %} is deprecated.  Please use " \
-    "{% load tentacles %} instead")
