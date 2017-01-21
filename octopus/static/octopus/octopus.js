87c02c0a839ecde23affad54c19e4d7108abe2da
Brian <brian.mcclr@gmail.com>
2017-01-21 08:14:54 +0100

    cleaned up JS a bit. added docblocks, made requests into a 'class', etc, etc

--- octopus/static/octopus/octopus.js
+++ octopus/static/octopus/octopus.js
@@ -7,112 +7,158 @@ $(function(){
         container: $('body').html()
     }, $('title').text(), window.location.pathname);
 
-    function link_helper(e){
-        if ($(this).attr('multi') == "False")
-            $(this).disable();
-
+    /**
+     * @param {Event} e
+     */
+    function prepareLinkRequest(e){
         e.preventDefault();
-        var insert = $(this).attr('insert');
-        request(this, insert, this.href);
+        
+        var link = this;
+        if (link.getAttribute('multi') == "False") {
+            $(link).disable();
+        }
+
+        var insertionMethod = link.getAttribute('insert');
+        var request = new Request(link, insertionMethod, this.href);
+
+        request.submit();
     }
 
-    function form_helper(e){
+    function prepareFormRequest(e){
         e.preventDefault();
-        var data = $(this).serializeArray();
-        request(this, $(this).attr('insert'), this.action, data);
+        var form = this;
+        var data = $(form).serializeArray();
+        var insertionMethod = form.getAttribute('insert');
+        var request = new Request(form, insertionMethod, this.action, data);
+
+        request.submit();
     }
 
     $.fn.extend({
         bindOctopus: function(){
-            $(this).find('.octopus-link').unbind('click', link_helper).bind('click', link_helper);
-            $(this).find('.octopus-form').unbind('submit', form_helper).bind('submit', form_helper);
+            $(this).find('.octopus-link').unbind('click', prepareLinkRequest).bind('click', prepareLinkRequest);
+            $(this).find('.octopus-form').unbind('submit', prepareFormRequest).bind('submit', prepareFormRequest);
             return this;
         },
         disable: function(){
             this.css({'pointer-events': 'none'});
+            $(this).unbind('click')
+                .removeClass('octopus-link')
+                .removeClass('octopus-form');
             return this;
         }
     });
 
-    function request(obj, insert, href, dataArray){
-        // Prevent undesirable multiple clicks on a link/element
-        if (href == location.href && $(obj).attr('multi') == "False")
-            return;
+    /**
+     *
+     * @param {HTMLElement} sourceElement
+     * @param {string} insertionMethod
+     * @param {string} href
+     * @param {Object} requestBody
+     */
+    function Request(sourceElement, insertionMethod, href, requestBody){
+        requestBody = requestBody || {};
 
-        var content = new String;
+        /**
+         * @type {string}
+         */
+        var content;
 
-        var error_content;  // container for error messages
-        var state = {};     // Dict to pass to pushState
-        dataArray = dataArray || {};
+        /**
+         * @type {string}
+         */
+        var errorContent;
 
-        $.ajax({
-            url: href,
-            type: $(obj).attr('method'),
-            data: dataArray
-        }).done(function(data){
-            title = $(obj).attr('title');
+        /**
+         *
+         * @type {Object}
+         */
+        var browserState = {};
 
-            switch(insert){
+        /**
+         * @type {string}
+         */
+        var title;
+
+        /**
+         * @param {string} newContent
+         */
+        var buildContent = function(newContent){
+            var oldContent = $(sourceElement.target).html();
+
+            title = $(sourceElement).attr('title');
+
+            switch(insertionMethod){
                 case "append":
-                    content = $(obj.target).html() + data;
+                    content = oldContent + newContent;
                     break;
                 case "prepend":
-                    content = data + $(obj.target).html();
+                    content = newContent + oldContent;
                     break;
                 case "replace":
                 default:
-                    content = data;
+                    content = newContent;
             }
 
-        }).fail(function( jqXHR, textStatus, errorThrown){
-            title = content = error_content = jqXHR.status+" "+errorThrown;
-        }).always(function(data){
-            data = !error_content ? data : error_content;
+        };
 
-            state = {
+        /**
+         * @param {Object} jqXHR
+         * @param {string} textStatus
+         * @param {string} errorThrown
+         */
+        var buildErrorContent = function(jqXHR, textStatus, errorThrown){
+            title = content = errorContent = jqXHR.status + " " + errorThrown;
+        };
+        var insertContent = function(data){
+            data = !errorContent ? data : errorContent;
+
+            browserState = {
                 title: title,
-                target: obj.target,
+                target: sourceElement.target,
                 container: content
             };
 
             var elem = $.parseHTML(data);
-            elem = $(elem).addClass("octopus-"+insert);
-            switch(insert){
+
+            elem = $(elem).addClass("octopus-"+insertionMethod);
+
+            switch(insertionMethod){
                 case 'prepend':
                     $(elem).hide();
-                    $(obj.target).prepend(elem);
+                    $(sourceElement.target).prepend(elem);
 
                     $(elem).slideDown("fast", function(){
-                        $(obj.target).bindOctopus();
+                        $(sourceElement.target).bindOctopus();
                     });
                     break;
                 case 'append':
                     $(elem).hide();
-                    $(obj.target).append(elem);
+                    $(sourceElement.target).append(elem);
 
                     $(elem).slideDown("fast", function(){
-                        $(obj.target).bindOctopus();
+                        $(sourceElement.target).bindOctopus();
                     });
 
                     break;
                 case 'self':
-                    if($(obj).hasClass('octopus-link')){
-                            obj = $(obj).parent();
+                    if($(sourceElement).hasClass('octopus-link')){
+                        sourceElement = $(sourceElement).parent();
                     }
-                    $(obj).fadeOut('fast', function() {
+                    $(sourceElement).fadeOut('fast', function() {
                         this.outerHTML = data;
                         $(this).fadeIn('fast', function(){
                             $('body').bindOctopus();
                         });
 
-                        state['target'] = obj;
+                        browserState['target'] = sourceElement;
                     });
                     break;
                 default:
 
-                    $(obj.target).fadeOut('fast', function() {
+                    $(sourceElement.target).fadeOut('fast', function() {
                         $(this).html(elem).fadeIn('fast', function(){
-                            $(obj.target).bindOctopus();
+                            $(sourceElement.target).bindOctopus();
                         });
                     });
 
@@ -121,25 +167,41 @@ $(function(){
                 $('title').text(title);
 
                 try{
-                    window.history.pushState(state, "", href);
+                    window.history.pushState(browserState, "", href);
                 }
                 catch(DataCloneError){
-                    state = {
+                    browserState = {
                         title: 'something went wrong, so we pushed everything',
                         target: 'body',
                         container: $('body').html()
                     };
-                    window.history.pushState(state, "", href);
+                    window.history.pushState(browserState, "", href);
                 }
             }
 
 
-        });
+        };
+
+        this.submit = function() {
+            // Prevent undesirable multiple clicks on a link/element
+            if (href == location.href && $(sourceElement).attr('multi') == "False") {
+                return;
+            }
+
+
+            $.ajax({
+                url: href,
+                type: $(sourceElement).attr('method'),
+                data: requestBody
+            }).done(buildContent)
+                .fail(buildErrorContent)
+                .always(insertContent);
+        }
     }
 
 
-    $('.octopus-link').on('click', link_helper);
-    $('.octopus-form').on('submit', form_helper);
+    $('.octopus-link').on('click', prepareLinkRequest);
+    $('.octopus-form').on('submit', prepareFormRequest);
 
     $(window).on('popstate', function(event) {
         var state = event.originalEvent.state;
