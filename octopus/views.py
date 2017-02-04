f6a19bb5f0aef52a6947c56ca648f0c6786b825d
Brian <brian.mcclr@gmail.com>
2017-02-04 06:29:53 +0100

    removed  template_fragment nonsense

--- octopus/views.py
+++ octopus/views.py
@@ -27,14 +27,10 @@ class AjaxResponseMixin(object):
 
     Be sure to set `parent` to the main class from which you're inheriting
     """
-    base_template = u'octopus/ajax.html'
-    ajax_template = u'octopus/ajax.html'
+    base_template = 'octopus/ajax.html'
+    ajax_template = 'octopus/ajax.html'
 
     parent = None
-
-    # Deprecated
-    fragment_name = None
-    fragment_suffix = None
 
     def get_context_data(self, **kwargs):
         context = {}
@@ -50,67 +46,7 @@ class AjaxResponseMixin(object):
         return context
 
 
-    def get_legacy_template_names(self, *args, **kwargs):
-        warnings.warn(deprecation_msg, DeprecationWarning)
-        if self.fragment_suffix is None:
-            self.fragment_suffix = '_fragment'
-
-        if self.request.is_ajax():
-            if self.fragment_name is not None:
-                self.template_name = self.fragment_name
-            self.template_name_suffix += self.fragment_suffix
-
-        if hasattr(self.parent, "get_template_names"):
-            return super(self.parent, self).get_template_names()
-
-        return [self.template_name, "%s%s"
-                % (self.template_name, self.template_name_suffix)]
-
-
-    def get_template_names(self):
-        if self.fragment_name is not None or self.fragment_suffix is not None:
-            return self.get_legacy_template_names()
-
-        if hasattr(self.parent, "get_template_names"):
-            return super(self.parent, self).get_template_names()
-
-        # hrm...I wonder why i did this...
-        return [self.template_name, "%s%s"
-                % (self.template_name, self.template_name_suffix)]
-
-
-class OctopusTemplateView(TemplateView):
-    base_template = 'octopus/ajax.html'
-    ajax_template = 'octopus/ajax.html'
-
-    fragment_name = None
-    fragment_suffix = None
-
-    def get_context_data(self, **kwargs):
-        template =  self.ajax_template if self.request.is_ajax() \
-            else self.base_template
-
-        return {'base_template': template}
-
-    def get_template_names(self):
-        if self.fragment_name is not None or self.fragment_suffix is not None:
-            return self.get_legacy_template_names()
-
-        return [self.template_name]
-
-    def get_legacy_template_names(self):
-        warnings.warn(deprecation_msg, DeprecationWarning)
-        if not self.request.is_ajax():
-            return [self.template_name, ]
-
-        if self.fragment_name is not None:
-            self.template_name = self.fragment_name
-        else:
-            self.template_name = self.template_name.replace(
-                ".", self.fragment_suffix + ".")
-
-
-views = (DetailView, ListView, DateDetailView, TodayArchiveView,
+views = (DetailView, TemplateView, ListView, DateDetailView, TodayArchiveView,
          DayArchiveView, WeekArchiveView, MonthArchiveView, YearArchiveView,
          ArchiveIndexView, FormView, CreateView, UpdateView, DeleteView, View)
 
